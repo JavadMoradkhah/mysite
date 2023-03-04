bae58578d124a3ce9056f02c67a5e9b1d687413c
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2023-03-04 21:58:54 +0330

    feat: Building the Testimonials component

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,38 @@
 import AOS from 'aos';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+
+const swiper = new Swiper('.testimonials-swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  centerInsufficientSlides: true,
+  allowTouchMove: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    hide: true,
+  },
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
 
 function isDarkMode() {
   return localStorage.getItem('darkMode') === 'true' ? true : false;
